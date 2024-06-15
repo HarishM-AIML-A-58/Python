dbf51b03dcd1e9a624be36e26444ab3629e58253
Harish M <231501058@rajalakshmi.edu.in>
2024-06-15 21:20:29 +0530

    .

---
+++ Programs/Week1.py
@@ -0,0 +1,34 @@
+#Done by M Harish AIML-A 231501058
+b=int(input(""))
+c=float(input(""))
+print("{0},{1}".format(b,type(b)))
+print("{:.1f},{}".format(c,type(c)))
+
+
+sal=int(input(""))
+dea=0.40*sal
+ren=0.20*sal
+print(round(sal+dea+ren))
+
+
+from math import sqrt
+a=float(input())
+print("{:.3f}".format(sqrt(a)))
+
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=a+b
+e=c-d
+print("{:.2f} is the gain percent.".format((e/d)*100))
+
+
+a=int(input())
+b=int(input())
+c=a*0.10
+d=b*0.25
+print("Your total refund will be ${:.2f}.".format(c+d))
+
+
+#Done by M Harish AIML-A 231501058
---
+++ Programs/Week12.py
@@ -0,0 +1,61 @@
+#Done by M Harish AIML-A 231501058
+import math
+n = int(input())
+print(n > 0 and math.log2(n).is_integer())
+
+
+import math
+diameter, tile_size = map(float, input().split())
+
+radius = diameter / 2
+area_pool = math.pi * (radius ** 2)
+tile_size_m = tile_size / 100
+area_tile = tile_size_m ** 2
+
+tiles_needed = math.ceil(area_pool / area_tile)
+print(f"{tiles_needed} tiles")
+
+
+
+no_of_shoes=int(input())
+size=input().split()
+size=[int(x) for x in size]
+customers=int(input())
+total=0
+for i in range(customers):
+    new=input().split()
+    new=[int(x) for x in new]
+    if new[0] in size:
+        size.remove(new[0])
+        total+=new[-1]
+print(total)
+ 
+
+
+
+n=int(input())
+l=input().split()
+l=[int(x) for x in l]
+k=int(input())
+sum=0
+for i in range(len(l)):
+    for j in range(i+1,len(l)):
+        if abs(l[i]-l[j])==k :
+            sum+=1
+print(sum)
+
+
+import math
+n = int(input())
+columns = input().split()
+marks_index = columns.index("MARKS")
+
+total_marks = 0
+for _ in range(n):
+    data = input().split()
+    total_marks += int(data[marks_index])
+
+average_marks = total_marks / n
+print(f"{average_marks:.2f}")
+
+ #Done by M Harish AIML-A 231501058
